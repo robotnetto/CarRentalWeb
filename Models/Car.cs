0c4e92cd8d8a310fd49aff455bb9aa063ab81994
Natthaphon Takhap <Jas39gripen@hotmail.se>
2023-02-21 12:29:36 +0100

    UserId

--- Models/Car.cs
+++ Models/Car.cs
@@ -3,10 +3,6 @@
     public class Car
     {
         public int CarId { get; set; }
-        public string Brand { get; set; } = "";
-        public string Model { get; set; } = "";
-        public string Color { get; set; } = "";
-        public int CarCategoryId { get; set; }
-
+        public string Model { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
         public string UserName { get; set; } = "";
         public string Password { get; set; } = "";
         public bool IsAdmin { get; set; } = false;
