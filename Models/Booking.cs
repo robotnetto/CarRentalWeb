e8de04d06cae0927239bff9089680efaf05f529d
Oskar Isaksson Aasa <oskar.isaksson.aasa@live.se>
2023-02-21 12:05:36 +0100

    Changed som props in models

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -6,5 +6,6 @@
         public int CarId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public int UserId { get; set; }
     }
 }
