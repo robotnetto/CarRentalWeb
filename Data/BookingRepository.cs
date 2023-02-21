d8546078e22d6c5e83ba44268177a83e0fba8b0a
Denise Jonsson <deenyze@hotmail.com>
2023-02-21 18:59:05 +0100

    Added booking conttroller and views

---
+++ Data/BookingRepository.cs
@@ -0,0 +1,42 @@
+﻿using Biluthyrning.Models;
+
+namespace Biluthyrning.Data
+{
+    public class BookingRepository : IBooking
+    {
+        private readonly CarRentalContext context;
+
+        public BookingRepository(CarRentalContext context)
+        {
+            this.context = context;
+        }
+        public void Create(Booking booking)
+        {
+            context.Bookings.Add(booking);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var booking = context.Bookings.FirstOrDefault(b => b.Id == id);
+            context.Bookings.Remove(booking);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Booking> GetAll()
+        {
+            return context.Bookings.OrderBy(s => s.Id);
+        }
+
+        public Booking GetById(int id)
+        {
+            return context.Bookings.FirstOrDefault(b => b.Id == id);
+        }
+
+        public void Update(Booking booking)
+        {
+            context.Bookings.Update(booking);
+            context.SaveChanges();
+        }
+    }
+}
