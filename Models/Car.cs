8816f7a42190b028a11f9afebdf23f90bd817efc
Oskar Isaksson Aasa <oskar.isaksson.aasa@live.se>
2023-02-21 13:50:18 +0100

    Made strings not null in Car

--- Models/Car.cs
+++ Models/Car.cs
@@ -3,9 +3,9 @@
     public class Car
     {
         public int CarId { get; set; }
-        public string Model { get; set; }
-        public string Brand { get; set; }
-        public string Color { get; set; }
+        public string Model { get; set; } = "";
+        public string Brand { get; set; } = "";
+        public string Color { get; set; } = "";
         public int CarCategoryId { get; set; }
 
     }
