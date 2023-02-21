c2ea13897c6a1dc37b6f76682d624cf549ec3a16
Oskar Isaksson Aasa <oskar.isaksson.aasa@live.se>
2023-02-21 15:07:04 +0100

    Added views, controller and repository for User.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Biluthyrning.Data;
+using Biluthyrning.Models;
+
+namespace Biluthyrning.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUser userRepo;
+        private readonly CarRentalContext _context;
+
+        public UsersController(IUser userRepo , CarRentalContext context)
+        {
+            this.userRepo = userRepo;
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            return View(userRepo.GetAll());
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || userRepo == null)
+            {
+                return NotFound();
+            }
+
+            var user = userRepo.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,UserName,Password,IsAdmin")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                userRepo.Add(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || userRepo == null)
+            {
+                return NotFound();
+            }
+
+            var user = userRepo.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,UserName,Password,IsAdmin")] User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    userRepo.Update(user);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || userRepo == null)
+            {
+                return NotFound();
+            }
+
+            var user = userRepo.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (userRepo == null)
+            {
+                return Problem("Entity set 'CarRentalContext.Users'  is null.");
+            }
+            var user = userRepo.GetById(id);
+            if (user != null)
+            {
+                userRepo.Delete(id);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+          return (_context.Users?.Any(e => e.UserId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Data/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Biluthyrning.Models;
+
+namespace Biluthyrning.Data
+{
+    public class UserRepository : IUser
+    {
+        private readonly CarRentalContext context;
+
+        public UserRepository(CarRentalContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+
+        public void Delete(int? id)
+        {
+            var userToDelete = GetById(id);
+            if (userToDelete != null)
+            {
+                context.Users.Remove(userToDelete);
+                context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return context.Users.OrderBy(x => x.UserId);
+        }
+
+        public User GetById(int? id)
+        {
+            return context.Users.First(x => x.UserId == id);
+        }
+
+        public void Update(User user)
+        {
+            context.Users.Update(user);
+            context.SaveChanges();
+        }
+    }
+}
