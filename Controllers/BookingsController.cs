dc154d7e1e021b142a3fcf215a4bae4b4a48c412
Denise Jonsson <deenyze@hotmail.com>
2023-02-22 17:25:19 +0100

    viewmodel för bookings/index och multipleAvtiveResultSets= true i connectionstring

--- Controllers/BookingsController.cs
+++ Controllers/BookingsController.cs
@@ -7,26 +7,44 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Biluthyrning.Data;
 using Biluthyrning.Models;
+using Biluthyrning.ViewModels;
 
 namespace Biluthyrning.Controllers
 {
     public class BookingsController : Controller
     {
         private readonly IBooking bookingRep;
+        private readonly ICar carRep;
+        private readonly IUser userRep;
 
-        public BookingsController(IBooking bookingRep)
+        public BookingsController(IBooking bookingRep, ICar carRep, IUser userRep)
         {
             this.bookingRep = bookingRep;
+            this.carRep = carRep;
+            this.userRep = userRep;
         }
 
         // GET: Bookings
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(bookingRep.GetAll());
+            var bookingVMList = new List<BookingViewModel>();
+            foreach (var item in bookingRep.GetAll())
+            {
+                var bvm = new BookingViewModel();
+                bvm.Id = item.Id;
+                bvm.CarId = item.CarId;
+                bvm.CarModel = carRep.GetById(item.CarId).Model;
+                bvm.CarBrand = carRep.GetById(item.CarId).Brand;
+                bvm.StartDate = item.StartDate;
+                bvm.EndDate = item.EndDate;
+                bvm.UserName = userRep.GetById(item.UserId).UserName;
+                bookingVMList.Add(bvm);
+            }
+            return View(bookingVMList);
         }
 
         // GET: Bookings/Details/5
-        public async Task<IActionResult> Details(int id)
+        public IActionResult Details(int id)
         {
             if (id == null)
             {
@@ -53,7 +71,7 @@ namespace Biluthyrning.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,CarId,StartDate,EndDate,UserId")] Booking booking)
+        public IActionResult Create([Bind("Id,CarId,StartDate,EndDate,UserId")] Booking booking)
         {
             if (ModelState.IsValid)
             {
@@ -64,7 +82,7 @@ namespace Biluthyrning.Controllers
         }
 
         // GET: Bookings/Edit/5
-        public async Task<IActionResult> Edit(int id)
+        public IActionResult Edit(int id)
         {
             if (id == null)
             {
@@ -84,7 +102,7 @@ namespace Biluthyrning.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,CarId,StartDate,EndDate,UserId")] Booking booking)
+        public IActionResult Edit(int id, [Bind("Id,CarId,StartDate,EndDate,UserId")] Booking booking)
         {
             if (id != booking.Id)
             {
@@ -114,7 +132,7 @@ namespace Biluthyrning.Controllers
         }
 
         // GET: Bookings/Delete/5
-        public async Task<IActionResult> Delete(int id)
+        public IActionResult Delete(int id)
         {
             if (id == null)
             {
@@ -133,7 +151,7 @@ namespace Biluthyrning.Controllers
         // POST: Bookings/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public IActionResult DeleteConfirmed(int id)
         {
             var booking = bookingRep.GetById(id);
             if (booking != null)
---
+++ ViewModels/BookingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Biluthyrning.ViewModels
+{
+    public class BookingViewModel
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public string CarModel { get; set; } = "";
+        public string CarBrand { get; set; } = "";
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string UserName { get; set; } = "";
+    }
+}
