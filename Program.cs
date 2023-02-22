c963f1fde03c2d912e238265bae1b9ee18a58b89
Linus Jakobsson <linus.jakobsson@yaelev.se>
2023-02-22 10:13:05 +0100

    Made the controller and Views for the CarCategory model

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace Biluthyrning
             builder.Services.AddControllersWithViews();
 
             builder.Services.AddDbContext<CarRentalContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppContext")));
+            builder.Services.AddTransient<ICarCategory, CarCategoryRepository>();
 
             var app = builder.Build();
 
