2ddb90c17db8043acb511eb9a53ed49b900aaac0
Natthaphon Takhap <Jas39gripen@hotmail.se>
2023-02-22 08:21:01 +0100

    CarControll&Repo

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -28,15 +28,14 @@ namespace Biluthyrning.Controllers
         }
 
         // GET: Cars/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public IActionResult (int id)
         {
-            if (id == null || _context.Cars == null)
+            if (id == null || carRepo == null)
             {
                 return NotFound();
             }
 
-            var car = await _context.Cars
-                .FirstOrDefaultAsync(m => m.CarId == id);
+            var car = carRepo.GetById(id);
             if (car == null)
             {
                 return NotFound();
@@ -56,26 +55,25 @@ namespace Biluthyrning.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CarId,Model,Brand,Color,CarCategoryId")] Car car)
+        public IActionResult Create([Bind("CarId,Model,Brand,Color,CarCategoryId")] Car car)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(car);
-                await _context.SaveChangesAsync();
+                carRepo.Create(car);
                 return RedirectToAction(nameof(Index));
             }
             return View(car);
         }
 
         // GET: Cars/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public  IActionResult Edit(int id)
         {
-            if (id == null || _context.Cars == null)
+            if (id! == null || carRepo == null)
             {
                 return NotFound();
             }
 
-            var car = await _context.Cars.FindAsync(id);
+            var car = carRepo.GetById(id);
             if (car == null)
             {
                 return NotFound();
@@ -88,7 +86,7 @@ namespace Biluthyrning.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CarId,Model,Brand,Color,CarCategoryId")] Car car)
+        public IActionResult Edit(int id, [Bind("CarId,Model,Brand,Color,CarCategoryId")] Car car)
         {
             if (id != car.CarId)
             {
@@ -99,19 +97,11 @@ namespace Biluthyrning.Controllers
             {
                 try
                 {
-                    _context.Update(car);
-                    await _context.SaveChangesAsync();
+                    carRepo.Update(car);
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (Exception)
                 {
-                    if (!CarExists(car.CarId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    return View();
                 }
                 return RedirectToAction(nameof(Index));
             }
@@ -119,15 +109,15 @@ namespace Biluthyrning.Controllers
         }
 
         // GET: Cars/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public IActionResult Delete(int id)
         {
-            if (id == null || _context.Cars == null)
+            if (id == null || carRepo == null)
             {
                 return NotFound();
             }
 
-            var car = await _context.Cars
-                .FirstOrDefaultAsync(m => m.CarId == id);
+            var car = carRepo.GetById(id);
+                
             if (car == null)
             {
                 return NotFound();
@@ -139,25 +129,19 @@ namespace Biluthyrning.Controllers
         // POST: Cars/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public IActionResult DeleteConfirmed(int id)
         {
-            if (_context.Cars == null)
+            if (carRepo == null)
             {
                 return Problem("Entity set 'CarRentalContext.Cars'  is null.");
             }
-            var car = await _context.Cars.FindAsync(id);
+            var car = carRepo.GetById(id);
             if (car != null)
             {
-                _context.Cars.Remove(car);
+                carRepo.Delete(car);
             }
             
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CarExists(int id)
-        {
-          return (_context.Cars?.Any(e => e.CarId == id)).GetValueOrDefault();
         }
     }
 }
