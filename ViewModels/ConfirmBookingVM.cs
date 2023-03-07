7bbf03ce4ac0cd5eee9ffcb4e929cdd85d5a7634
natthaphon takhap <Jas39gripen@hotmail.se>
2023-03-07 09:04:04 +0100

    bla

---
+++ ViewModels/ConfirmBookingVM.cs
@@ -0,0 +1,25 @@
+﻿using Biluthyrning.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Build.Framework;
+
+namespace Biluthyrning.ViewModels
+{
+    public class ConfirmBookingVM
+    {
+        [Required]
+        public int CarId { get; set; }
+        public string CarModel { get; set; } = "";
+        public string CarBrand { get; set; } = "";
+        [Required]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
+        public string UserName { get; set; } = "";
+        [Required]
+        public int UserId { get; set; }
+        public string CarCategoryName { get; set; } = "";
+        public decimal Price { get; set; }
+        public decimal TotalCost { get; set; }
+        public List<Car> Cars { get; set; } = new();
+    }
+}
