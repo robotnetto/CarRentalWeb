d75c09a732db3d7436a5e4a7f8ed1888e4ab81ed
Oskar Isaksson Aasa <oskar.isaksson.aasa@live.se>
2023-03-14 10:15:53 +0100

    Added DisplayName in User model

--- Models/User.cs
+++ Models/User.cs
@@ -6,13 +6,16 @@ namespace Biluthyrning.Models
     {
         public int UserId { get; set; }
         [Required, MinLength(2), MaxLength(15)]
+        [Display(Name = "Username")]
         public string UserName { get; set; } = "";
         [Required, MinLength(8), MaxLength(20)]
         public string Password { get; set; } = "";
+        [Display(Name = "Administrator")]
         public bool IsAdmin { get; set; } = false;
         [Required]
         public string Email { get; set; } = "";
         [Required]
+        [Display(Name = "Phone")]
         public int PhoneNr { get; set; }
         public List<Booking> Bookings { get; set; } = new List<Booking>();
     }
